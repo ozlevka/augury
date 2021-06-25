ef7f57b3d95929d96ec8d62c99f8643f02c5adb2
Lev <ozlevka@gmail.com>
2021-06-25 20:50:47 +0300

    Find branch name

--- vars/kubernetes.groovy
+++ vars/kubernetes.groovy
@@ -4,11 +4,24 @@ def call() {
     
     podTemplate(yaml: podYaml) {
         node(POD_LABEL) {
+            makeProperties()
             stage("Run shell hello") {
                 container("ubuntu-test") {
                     sh "echo Hello container"
                 }
             }
         }
+    }
+}
+
+def makeProperties() {
+    // properties([
+    //     parameters([
+    //         string(name: 'SERVICE_NAME', 
+    //         ], description: 'The target environment', )
+    //     ])
+    // ])
+    stage("The Test") {
+        echo "The branch ${env.BRANCH_NAME}"
     }
 }
