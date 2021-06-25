2e5bbe9611490135cc8fa7821a277647e5b6e9d7
Lev <ozlevka@gmail.com>
2021-06-25 20:54:22 +0300

    Add property

--- vars/kubernetes.groovy
+++ vars/kubernetes.groovy
@@ -1,10 +1,9 @@
 
 def call() {
     def podYaml = libraryResource "org/ozlevka/defaultPod.yaml"
-    
+    makeProperties()
     podTemplate(yaml: podYaml) {
         node(POD_LABEL) {
-            makeProperties()
             stage("Run shell hello") {
                 container("ubuntu-test") {
                     sh "echo Hello container"
@@ -15,13 +14,9 @@ def call() {
 }
 
 def makeProperties() {
-    // properties([
-    //     parameters([
-    //         string(name: 'SERVICE_NAME', 
-    //         ], description: 'The target environment', )
-    //     ])
-    // ])
-    stage("The Test") {
-        echo "The branch ${env.BRANCH_NAME}"
-    }
+    properties([
+        parameters([
+            string(name: 'BRANCH_NAME', description: 'Branch name to build', )
+        ])
+    ])
 }
