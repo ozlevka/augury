1435aa0ab111014f7aeb9eec54c63b3c5cf0a72c
Lev <ozlevka@gmail.com>
2021-06-25 20:45:27 +0300

    Try fix syntax

--- vars/kubernetes.groovy
+++ vars/kubernetes.groovy
@@ -1,12 +1,14 @@
 
 def call() {
-    podTemplate(yaml: libraryResource "org/ozlevka/defaultPod.yaml") {
-    node(POD_LABEL) {
-        stage("Run shell hello") {
-            container("ubuntu-test") {
-                sh "echo Hello container"
+    def podYaml = libraryResource "org/ozlevka/defaultPod.yaml"
+    
+    podTemplate(yaml: podYaml) {
+        node(POD_LABEL) {
+            stage("Run shell hello") {
+                container("ubuntu-test") {
+                    sh "echo Hello container"
+                }
             }
         }
     }
-  }
 }
