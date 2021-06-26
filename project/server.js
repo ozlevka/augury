218f5a740a9c94d9ac43356edeb26be944bcd189
Lev <ozlevka@gmail.com>
2021-06-26 08:49:54 +0300

    Project

---
+++ project/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/test", (req, res) => {
+    res.send("OK");
+});
+
+app.listen(port, '0.0.0.0', () => {
+   console.log(`Example app listening at http://0.0.0.0:${port}`)
+})
