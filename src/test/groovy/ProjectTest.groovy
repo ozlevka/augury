6866ce1efbe6f767c82ca03781d9df7cdf5743b5
Lev <ozlevka@gmail.com>
2021-06-26 08:13:07 +0300

    Save changes + terraform

---
+++ src/test/groovy/ProjectTest.groovy
@@ -0,0 +1,12 @@
+package test.groovy
+
+import org.junit.Test
+import org.testng.Assert
+
+class ProjectTest {
+
+    @Test
+    void testOfTest() {
+        Assert.assertEquals(true, true)
+    }
+}
