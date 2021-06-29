668531bcae94b3390634827e7f62a6d612a2f4b8
Lev <lev@Levs-MacBook-Pro.local>
2021-06-29 19:53:56 +0300

    Created terraform for infra

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,5 +17,69 @@ resource "digitalocean_droplet" "jenkins_server" {
   name     = "jenkins"
   region   = "ams3"
   size     = "s-1vcpu-1gb"
-  ssh_keys = [30371814, 30684983] 
+  ssh_keys = [30371814, 30684983]
+
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "ufw disable",
+      "echo ${var.github_token} | docker login ghcr.io/ozlevka -u ozlevka --password-stdin",
+      "docker run -d -v ~/.kube:/root/.kube --network host ghcr.io/ozlevka/jenkins-empty:v0.0.4"
+    ]
+  } 
 }
+
+resource "digitalocean_firewall" "jenkins_firewall" {
+  name = "augury-test-terraform"
+  droplet_ids = [digitalocean_droplet.jenkins_server.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "8080"
+    source_addresses = ["140.82.112.0/20", "143.55.64.0/20", "185.199.108.0/22", "192.30.252.0/22"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "1-65535"
+    source_addresses = ["10.133.0.0/24", "84.229.91.149"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+resource "digitalocean_kubernetes_cluster" "augury_test" {
+  name    = "augury-test"
+  region  = "ams3"
+  version = "1.19.12-do.0"
+
+  node_pool {
+    name       = "augury-worker-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+  }
+}
+
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,16 @@ variable do_token {
   type        = string
   description = "description"
 }
+
+variable github_token {
+  type        = string
+  description = "Github token for login to ghcr.io"
+}
+
+variable pvt_key {
+  type        = string
+  default     = "/home/vscode/.ssh/do"
+  description = "Pathio to private key"
+}
+
+
